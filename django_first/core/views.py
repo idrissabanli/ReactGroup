bf23105e5976a59915d8e59cacd583ebce34154d
idris <idris.sabanli@gmail.com>
2020-06-03 21:18:23 +0400

    statis files

--- django_first/core/views.py
+++ django_first/core/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def index(request):
-    return render(request, 'index.html')
+    name = 'idris'
+    context = {
+        'user': name,
+        'now': datetime.now()
+    }
+    return render(request, 'index.html', context)
+
+def about(request):
+    return render(request, 'about.html')
